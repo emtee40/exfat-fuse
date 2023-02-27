webmaster33/exfat

Implement "showexec" mount option.
This option allows execute permission bits only for files with .EXE,
.COM, or .BAT extension.

libexfat/mount.c
@@ -103,6 +103,7 @@ static void parse_options(struct exfat* ef, const char* options)
 	ef->gid = get_int_option(options, "gid", 10, getegid());
 
 	ef->noatime = match_option(options, "noatime");
+	ef->showexec = match_option(options, "showexec");
 }
 
 static bool verify_vbr_checksum(struct exfat_dev* dev, void* sector,

emtee40/exfat-fuse

libexfat/mount.c
@@ -91,6 +91,7 @@ static void parse_options(struct exfat* ef, const char* options)
 	ef->gid = get_int_option(options, "gid", 10, getegid());
 
 	ef->noatime = exfat_match_option(options, "noatime");
+	ef->showexec = exfat_match_option(options, "showexec");
 
 	switch (get_int_option(options, "repair", 10, 0))
 	{